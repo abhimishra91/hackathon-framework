5aa5b8fafade9ba05f99f3f71f42ca3db89badda
Abhishek Kumar Mishra <abhimishra.91@gmail.com>
2020-05-27 11:17:16 +0800

    metrics updated with regression

--- metrics.py
+++ metrics.py
@@ -1,5 +1,49 @@
 from sklearn import metrics as skmetrics
+import numpy as np
 
+
+
+
+class RegressionMetric:
+    def __init__(self):
+        self.metrics = {
+            "mae":self._mae,
+            "mse":self._mse,
+            "rmse":self._rmse,
+            "msle":self._msle,
+            "rmsle":self._rmsle,
+            "r2":self._r2
+        }
+
+    def __call__(self, metric, y_true, y_pred):
+        if metric not in self.metrics:
+            raise Exception("Metric not implemented")
+        else:
+            return self.metrics[metric](y_true=y_true, y_pred=y_pred)
+
+    @staticmethod
+    def _mae(y_true, y_pred):
+        return skmetrics.mean_absolute_error(y_true=y_true, y_pred=y_pred)  
+
+    @staticmethod
+    def _mse(y_true, y_pred):
+        return skmetrics.mean_squared_error(y_true=y_true, y_pred=y_pred)
+
+    def _rmse(self, y_true, y_pred):
+        return np.sqrt(self._mse(y_true=y_true, y_pred=y_pred))
+
+    @staticmethod
+    def _msle(y_true, y_pred):
+        return skmetrics.mean_squared_log_error(y_true=y_true, y_pred=y_pred)
+
+    def _rmsle(self, y_true, y_pred):
+        return np.sqrt(self._msle(y_true=y_true, y_pred=y_pred))
+
+    @staticmethod
+    def _r2(y_true, y_pred):
+        return skmetrics.r2_score(y_true=y_true, y_pred=y_pred)
+
+    
 
 class ClassificationMetric:
     def __init__(self):
